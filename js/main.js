7878511a733eb2f76a55f0570e633a7d098eefeb
jules-desplenter <jules.desplenter1@gmail.com>
2021-12-18 17:09:07 +0100

    vcf dink 2.0

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 
-function addcontact() {
+function addcontact(data) {
   console.log("done");
   let name = data.name.split(' ')[0]
   let surname = name[0];
@@ -7,16 +7,8 @@ function addcontact() {
   for(let i = 1; i < name.length(); i++){
     behindname += name[i] + " ";
   }
-  var data = `BEGIN:VCARD
-  VERSION:3.0
-  N:Desplenter;Jules;;;
-  FN:Desplenter Jules
-  ORG:Ugent;
-  EMAIL;type=INTERNET;type=HOME;type=pref:jules.desplenter1@gmail.com
-  TEL;type=CELL;type=VOICE;type=pref:+32477329979
-  NOTE:Student industrieel ingenieur
-  BDAY:2000-26-10
-  END:VCARD`;
+  // %3A = dubbelpunt %0A = enter %3B = ; %3D = =
+  var data = `BEGIN%3AVCARD%0AVERSION%3A3.0%0AN%3A${behindname}%3B${surname}%3B%3B%3B%0AFN%3A${data.name}%0ATEL%3Btype%3DCELL%3Btype%3DVOICE%3Btype%3Dpref%3A${data.phone}%0ANOTE%3A${data.bio}%OAEND%3AVCARD`;
   window.open("data:text/x-vcard;urlencoded," + data);
 }
 
@@ -25,6 +17,7 @@ const place_stuff = (data) => {
   document.getElementById("name").innerHTML = data.name;
   document.getElementById("bio").innerHTML = data.bio;
   document.getElementById("picture").src = data.picture;
+  document.getElementById("contact").addEventListener("click",() => addcontact(data));
   links = [];
   links.push({ link: data.facebook, importance: data.facebook_importance, social: "facebook" });
   links.push({ link: data.instagram, importance: data.instagram_importance, social: "instagram" });
@@ -47,7 +40,6 @@ const place_stuff = (data) => {
 
 
 const init = async () => {
-  document.getElementById("contact").addEventListener("click",addcontact);
   fetch("/js/test.json")
     .then((response) => {
       return response.json();
