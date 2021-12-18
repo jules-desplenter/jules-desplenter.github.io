81477ef851654e1144310172a462bef87f034fa0
jules-desplenter <jules.desplenter1@gmail.com>
2021-12-18 13:57:48 +0100

    contact enal

--- js/main.js
+++ js/main.js
@@ -1,20 +1,20 @@
 
 const place_stuff = (data) => {
-    document.getElementById("name").innerHTML = data.name;
-    document.getElementById("bio").innerHTML = data.bio;
-    document.getElementById("picture").src = data.picture;
-    links = [];
-    links.push({link:data.phone,importance:data.phone_importance,social:"phone" });
-    links.push({link:data.facebook,importance:data.facebook_importance,social:"facebook" });
-    links.push({link:data.instagram,importance:data.instagram_importance,social:"instagram" });
-    links.push({link:data.linkedin,importance:data.linkedin_importance,social:"linkedin" });
-    links.sort(function(a,b) {
-        return b.importance - a.importance
-    });
-    console.log(links);
-    html = ''
-  for(i = 0; i < links.length; i++){
-      html += `<a
+  document.getElementById("name").innerHTML = data.name;
+  document.getElementById("bio").innerHTML = data.bio;
+  document.getElementById("picture").src = data.picture;
+  links = [];
+  links.push({ link: data.facebook, importance: data.facebook_importance, social: "facebook" });
+  links.push({ link: data.instagram, importance: data.instagram_importance, social: "instagram" });
+  links.push({ link: data.linkedin, importance: data.linkedin_importance, social: "linkedin" });
+  links.sort(function (a, b) {
+    return b.importance - a.importance
+  });
+
+
+  html = ''
+  for (i = 0; i < links.length; i++) {
+    html += `<a
       class="c-main__links-items"
       href="${links[i].link}"
       ><img class="c-pic" src="./images/icons8-${links[i].social}.svg" alt="facebook logo"
