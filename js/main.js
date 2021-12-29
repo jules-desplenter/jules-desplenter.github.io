221284f4395706e6208ef671eeb5d7f6f4e2a2d9
jules-desplenter <jules.desplenter1@gmail.com>
2021-12-29 19:51:22 +0100

    test azure

--- js/main.js
+++ js/main.js
@@ -63,9 +63,9 @@ const init = async () => {
   data = {}
   const response = await fetch(url, {
     method: 'POST', // *GET, POST, PUT, DELETE, etc.
-    mode: 'cors', // no-cors, *cors, same-origin
+    // mode: 'cors', // no-cors, *cors, same-origin
     cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-    credentials: 'same-origin', // include, *same-origin, omit
+    // credentials: 'same-origin', // include, *same-origin, omit
     headers: {
       'Content-Type': 'application/json'
       // 'Content-Type': 'application/x-www-form-urlencoded',
