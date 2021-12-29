8699683d679ad90b3ae78221c83fffa059ccedb8
jules-desplenter <jules.desplenter1@gmail.com>
2021-12-29 20:19:07 +0100

    test azure

--- js/main.js
+++ js/main.js
@@ -61,20 +61,23 @@ const init = async () => {
   console.log(params.id);
   url = `https://dotdbelgium.azurewebsites.net/api/getuser?code=XJ312iaiqxMTfwLqPCyzPNU6MJkPMIhTDVCiiMWihcmOQ01cPxUi5g==&id=${params.id}`
   data = {}
-  const response = await fetch(url, {
+  fetch(url, {
     method: 'GET', // *GET, POST, PUT, DELETE, etc.
-    mode: 'no-cors', // no-cors, *cors, same-origin
-    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+    // mode: 'cors', // no-cors, *cors, same-origin
+    // cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
     // credentials: 'same-origin', // include, *same-origin, omit
     headers: {
       'Content-Type': 'application/json'
       // 'Content-Type': 'application/x-www-form-urlencoded',
     },
-    redirect: 'follow', // manual, *follow, error
-    referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-    body: JSON.stringify(data) // body data type must match "Content-Type" header
-  });
-  place_stuff(response.body)
+    // redirect: 'follow', // manual, *follow, error
+    // referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+  }).then((response) => {
+    console.log(response);
+    return response.json();
+  })
+  .then((data) => {console.log(data)})
+  // place_stuff(response.body);
 };
 
 document.addEventListener("DOMContentLoaded", init);
