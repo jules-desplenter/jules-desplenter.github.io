aa51faa36593e0e6803c5705d6503d2ff589e1e9
jules-desplenter <jules.desplenter1@gmail.com>
2022-04-20 15:03:32 +0200

    test

--- js/main.js
+++ js/main.js
@@ -16,7 +16,7 @@ function addcontact(data) {
     window.open("data:text/x-vcard;urlencoded," + data);
   } else {
     if (/android/i.test(userAgent)) {
-      window.open(data);
+     navigator.share(data);
     } else {
       window.alert(
         "this function is not available on computer but here is the phone number: " +
