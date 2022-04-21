282413407e4a8698b17f3a0e660b891520871e15
jules-desplenter <jules.desplenter1@gmail.com>
2022-04-21 11:30:05 +0200

    fix phonenumber

--- js/main.js
+++ js/main.js
@@ -16,7 +16,7 @@ function addcontact(data) {
     window.open("data:text/x-vcard;urlencoded," + data);
   } else {
     if (/android/i.test(userAgent)) {
-     window.open("tel:" + data.phone)
+     window.open("tel:" + number)
     } else {
       window.alert(
         "this function is not available on computer but here is the phone number: " +
